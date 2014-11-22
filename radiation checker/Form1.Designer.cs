b739d61603665aa299b4e29b34f382ebf030ba66
Alex Restifo <calix1999@gmail.com>
2014-11-22 17:44:54 -0500

    Some fixes

--- radiation checker/Form1.Designer.cs
+++ radiation checker/Form1.Designer.cs
@@ -335,10 +335,10 @@
             this.Controls.Add(this.ele1MassNum);
             this.Controls.Add(this.ele1Label);
             this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Decay calculator";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
